90656e2390b3a0cd5f69eec42adb1a131de7c00f
Aleksandra-Mira <64834473+Aleksandra-Mira@users.noreply.github.com>
2022-01-28 08:33:59 +0100

    Create all variables needed in game
    try_num - counter for amount of guesses
    try_max - The maximum amount of guesses
    max - maximum number
    min - minimum number
    guess - if computer win the game
    ans - variable to store answer if guessed number is higher or lower

---
+++ src/main.py
@@ -0,0 +1,10 @@
+print('Welcome in GUESS the number game')
+try_num = 0
+max = 100
+min = 0
+guess = False
+try_max = 0
+ans = ''
+print('Choose your number from ', min, ' to ', max, ' . (Don\'t tell us!)')
+try_max = int(input("How many tries I have? Please don't tell 0 :) "))
+input('Enter to close the console')
