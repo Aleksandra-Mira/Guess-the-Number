90510b4997ada20f225728f0e32b696242122871
Aleksandra-Mira <64834473+Aleksandra-Mira@users.noreply.github.com>
2022-01-28 08:46:56 +0100

    winner verification

--- src/main.py
+++ src/main.py
@@ -7,4 +7,11 @@ try_max = 0
 ans = ''
 print('Choose your number from ', min, ' to ', max, ' . (Don\'t tell us!)')
 try_max = int(input("How many tries I have? Please don't tell 0 :) "))
+while (not guess and try_num != try_max):
+    try_num += 1
+if(not guess):
+    print('I lost :( I run of guess number.')
+else:
+    print('I won! I\'m much clever than you! LOL')
+    print('I just needed ', try_num, ' tries to guess your num.')
 input('Enter to close the console')
